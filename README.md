2383f8e7ef1468d79b5c0f056f3df07ed8099545
Dj Walker-Morgan <codepope@gmail.com>
2016-09-22 11:27:32 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# compose-elasticsearch-helloworld-nodejs overview
+
+compose-elasticsearch-helloworld-nodejs is a sample Bluemix application which shows you how to connect to an IBM Compose for Elasticsearch for Bluemix service using Node.js.
+
+## Running the app on Bluemix
+
+1. If you do not already have a Bluemix account, [sign up here][bluemix_signup_url]
+
+2. Download and install the [Cloud Foundry CLI][cloud_foundry_url] tool
+
+3. Clone the app to your local environment from your terminal using the following command:
+
+  ```
+  git clone https://github.com/IBM-Bluemix/compose-elasticsearch-helloworld-nodejs.git
+  ```
+
+4. `cd` into this newly created directory
+
+5. Open the `manifest.yml` file and change the `host` value to something unique. The host you choose will determinate the subdomain of your application's URL:  `<host>.mybluemix.net`
+
+6. Connect to Bluemix in the command line tool and follow the prompts to log in.
+
+  ```
+  $ cf api https://api.ng.bluemix.net
+  $ cf login
+  ```
+
+7. If you don't already have a Compose for Elasticsearch service in Bluemix, create one.
+
+  **Note :** The Compose for Elasticsearch service does not offer a free plan. For details of pricing, see the _Pricing Plans_ section of the [Compose for Elasticsearch service][compose_for_elasticsearch_url] in Bluemix.
+
+  ```
+  $ cf create-service compose-elasticsearch-helloworld-nodejs Standard my-compose-for-elasticsearch-service
+  ```
+
+8. Push the app to Bluemix.
+
+  ```
+  $ cf push
+  ```
+
+Your application is now running at `<host>.mybluemix.net`.
+
+The node-elasticsearch-helloworld app displays the contents of an _examples_ Elasticsearch index. To demonstrate that the app is connected to your service, add some words to the index. The words are displayed as you add them, with the most recently added words displayed first.
+
+## Code Structure
+
+| File | Description |
+| ---- | ----------- |
+|[**server.js**](server.js)|Establishes a connection to the Elasticsearch index using credentials from VCAP_ENV and handles create and read operations on the index. |
+|[**main.js**](public/javascripts/main.js)|Handles user input for a PUT command and parses the results of a GET command to output the contents of the Elasticsearch index.|
+
+The app uses a PUT and a GET operation:
+
+- PUT
+  - takes user input from [main.js](public/javascript/main.js)
+  - uses the `client.index` method to add user input to the index
+
+- GET
+  - uses the `client.search` method to retrieve the contents of the _words_ type
+  - returns the response to [main.js](public/javascript/main.js)
+
+## Privacy Notice
+The sample web application includes code to track deployments to Bluemix and other Cloud Foundry platforms. The following information is sent to a [Deployment Tracker](https://github.com/cloudant-labs/deployment-tracker) service on each deployment:
+
+* Application Name (application_name)
+* Space ID (space_id)
+* Application Version (application_version)
+* Application URIs (application_uris)
+
+This data is collected from the VCAP_APPLICATION environment variable in IBM Bluemix and other Cloud Foundry platforms. This data is used by IBM to track metrics around deployments of sample applications to IBM Bluemix. Only deployments of sample applications that include code to ping the Deployment Tracker service will be tracked.
+
+### Disabling Deployment Tracking
+
+Deployment tracking can be disabled by removing `require("cf-deployment-tracker-client").track();` from the beginning of the `server.js` file.
+
+[compose_for_elasticsearch_url]: https://console.ng.bluemix.net/catalog/services/compose-for-elasticsearch/
+[bluemix_signup_url]: https://ibm.biz/compose-for-elasticsearch-signup
+[cloud_foundry_url]: https://github.com/cloudfoundry/cli
---
+++ server.js
@@ -0,0 +1,136 @@
+/**
+ * Copyright 2016 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ // First add the obligatory web framework
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Util is handy to have around, so thats why that's here.
+const util = require('util')
+// and so is assert
+const assert = require('assert');
+
+// We want to extract the port to publish our app on
+var port = process.env.VCAP_APP_PORT || 8080;
+
+// Then we'll pull in the database client library
+var elasticsearch=require('elasticsearch');
+
+// Now lets get cfenv and ask it to parse the environment variable
+var cfenv = require('cfenv');
+var appenv = cfenv.getAppEnv();
+
+// Within the application environment (appenv) there's a services object
+var services = appenv.services;
+
+// The services object is a map named by service so we extract the one for PostgreSQL
+var es_services = services["compose-for-elasticsearch"];
+
+// This check ensures there is a services for Elasticsearch databases
+assert(!util.isUndefined(es_services), "Must be bound to compose-for-elasticsearch services");
+
+// We now take the first bound MongoDB service and extract it's credentials object
+var credentials = es_services[0].credentials;
+
+// Within the credentials, an entry ca_certificate_base64 contains the SSL pinning key
+// We convert that from a string into a Buffer entry in an array which we use when
+// connecting.
+var ca = new Buffer(credentials.ca_certificate_base64, 'base64');
+
+var client = new elasticsearch.Client( {
+  hosts: [
+    credentials.uri,
+    credentials.uri_direct_1
+  ],
+  ssl: {
+    ca: ca
+  }
+});
+
+client.indices.exists({
+  index:'examples'
+},function(err,resp,status) {
+  if (resp === false) {
+    client.indices.create({
+      index: 'examples'
+    },function(err,resp,status) {
+      if(err) {
+        console.log(err);
+      }
+    });
+  }
+});
+
+// We can now set up our web server. First up we set it to serve static pages
+app.use(express.static(__dirname + '/public'));
+
+app.put("/words", function(request, response) {
+  var now = new Date();
+  client.index({
+    index: 'examples',
+    type: 'words',
+    body: {
+      "word": request.body.word,
+      "definition": request.body.definition,
+      "added": now
+    }
+  },function(err,resp,status) {
+    if (err) {
+      response.status(500).send(err);
+    } else {
+      response.send(resp);
+    }
+  });
+});
+
+// Read from the database when someone visits /hello
+app.get("/words", function(request, response) {
+
+  client.search({
+    index: 'examples',
+    type: 'words',
+    fields: ['word','definition'],
+    body: {
+      sort: {
+        'added' : {
+          order: 'desc'
+        }
+      }
+    }
+  },function (err,resp,status) {
+    if (err) {
+      response.status(500).send(err);
+    } else {
+      // get the words from the index
+      var words = [];
+      resp.hits.hits.forEach(function(hit){
+        words.push( { "word" : hit.fields.word , "definition" : hit.fields.definition } );
+      });
+      response.send(words);
+    }
+  });
+
+});
+
+// Now we go and listen for a connection.
+app.listen(port);
+
+require("cf-deployment-tracker-client").track();
